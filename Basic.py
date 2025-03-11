8a29304aea054222638e8248eba4db123e1eed15
SravaniKolanu <sravani.k9122@gmail.com>
2025-03-10 22:52:21 -0400

    updated

---
+++ Basic.py
@@ -0,0 +1,62 @@
+# Area of Circle Pir2
+# pi=3.14159
+# radius=int(input("Enter the radius of circle:"))
+# Area=pi*radius*radius
+# print("Area of Circle:",Area)
+
+# Circumference of the Circle 2Pir
+# pi=3.14159
+# radius=int(input("Enter the radius of circle:"))
+# Circumference=2*pi*radius
+# print("Circumference of the Circle:",Circumference)
+
+# Volume of a Cylinder Pir2h
+# pi=3.14159
+# radius=int(input("Enter the radius of the Cylinder:"))
+# height=int(input("Enter the height of the Cylinder:"))
+# Volume=(pi*radius*radius*height)
+# print("Volume of a Cylinder:",Volume)
+
+# Surface Area of a Cylinder 2Pirh+2Pir2
+# pi=3.14159
+# radius=int(input("Enter the radius of the Cylinder:"))
+# height=int(input("Enter the height of the Cylinder:"))
+# Surface=(2*pi*radius*height+2*pi*radius*radius)
+# print("Surface Area of a Cylinder:",Surface)
+
+#Area of the rectangle L*W
+# lenght=int(input("Enter the lenght of rectangle:"))
+# width=int(input("Enter the width of a rectangle:"))
+# Area=(lenght*width)
+# print("Area of the rectangle:",Area)
+
+#Perimeter of the rectangle 2(L*W)
+# lenght=int(input("Enter the lenght of rectangle:"))
+# width=int(input("Enter the width of a rectangle:"))
+# Perimeter=(2*(lenght*width))
+# print("Perimeter of the rectangle:",Perimeter)
+
+# Area of the Square side2
+# side=int(input("Enter the Side lenght of square:"))
+# Area=(side*side)
+# print("Area of the Square:",Area)
+
+
+# Comparison Operators (>, <, , >=, <=,  ==,  !=)
+# Greater than
+# Less than
+# Greater than or equal to
+# Less than or equal to
+# Equal to
+# Not equal to
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("a > b:", a > b)   
+print("a < b:", a < b)   
+print("a >= b:", a >= b) 
+print("a <= b:", a <= b) 
+print("a == b:", a == b) 
+print("a != b:", a != b)
+
+
